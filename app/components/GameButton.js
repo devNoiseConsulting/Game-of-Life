c2c28432a3b42208a7db495a9277186e7f5dcfc5
Michael J. Flynn <flynnmj@devnoise.com>
2017-03-27 12:16:06 -0400

    Button fix
    Adding some bootstrap classes to the buttons.

--- app/components/GameButton.js
+++ app/components/GameButton.js
@@ -10,7 +10,7 @@ var GameButton = React.createClass({
 
     render: function() {
         return (
-          <button onClick={this.props.onClick}>{this.props.text}</button>
+          <button className="btn btn-default btn-block" onClick={this.props.onClick}>{this.props.text}</button>
         );
     }
 });
